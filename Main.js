859aa448cfeea73e4f68dfe5543ff5a859b8c7fe
William Ferns <willferns2000@gmail.com>
2024-01-16 14:47:13 +0200

    Made empty calendar table on initial document creation on Main.js

--- Main.js
+++ Main.js
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
-import { SignedIn, SignedOut, useSession, useUser } from "@clerk/clerk-expo";
-import Constants from "expo-constants"
+import { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useSession, useUser } from "@clerk/clerk-expo";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDoc, setDoc } from "firebase/firestore";
+
 import SignUpScreen from "./components/SignUpScreen";
 import Starter from "./components/Starter";
 import Home from "./components/Home";
 import SignUp from "./components/SignUpScreen";
 import SignIn from "./components/SignInScreen";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import firebaseConfig from "./firebaseConfig"
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import firebaseConfig from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -21,66 +19,69 @@ const db = getFirestore(app);
 const Stack = createStackNavigator();
 
 export default function Main() {
-    const { loading, session } = useSession();
-    const { user } = useUser();
+  const { loading, session } = useSession();
+  const { user } = useUser();
 
-    useEffect(() => {
-        async function checkAndCreateDocument(db, collectionName, documentID, data) {
-            try {
-                // Check if the document ID is null or undefined
-                if (!documentID) {
-                    console.log("Document ID is null or undefined.");
-                    return;
-                }
-
-                const collectionRef = collection(db, collectionName);
-                const documentRef = doc(collectionRef, documentID);
-
-                // Check if the document exists
-                const documentSnapshot = await getDoc(documentRef);
-
-                if (documentSnapshot.exists()) {
-                    console.log(`Document with ID ${documentID} already exists.`);
-                } else {
-                    // Document doesn't exist, create a new one
-                    await setDoc(documentRef, data);
-                    console.log(`New document with ID ${documentID} created.`);
-                }
-            } catch (error) {
-                console.error("Error checking/creating document:", error);
-            }
+  useEffect(() => {
+    async function checkAndCreateDocument(db, collectionName, documentID, data) {
+      try {
+        // Check if the document ID is null or undefined
+        if (!documentID) {
+          console.log("Document ID is null or undefined.");
+          return;
         }
 
-        // Usage example:
-        const collectionName = "users";
-        const documentID = user?.id;
-        const data = {
-            userId: user?.id || "",
-            // Add other data properties as needed
-        };
+        const collectionRef = collection(db, collectionName);
+        const documentRef = doc(collectionRef, documentID);
 
-        checkAndCreateDocument(db, collectionName, documentID, data);
+        // Check if the document exists
+        const documentSnapshot = await getDoc(documentRef);
 
-    }, [loading, session, user])
+        if (documentSnapshot.exists()) {
+          console.log(`Document with ID ${documentID} already exists.`);
+        } else {
+          // Document doesn't exist, create a new one
+          await setDoc(documentRef, data);
+          console.log(`New document with ID ${documentID} created.`);
+        }
+      } catch (error) {
+        console.error("Error checking/creating document:", error);
+      }
+    }
+
+    // Usage example:
+    const collectionName = "users";
+    const documentID = user?.id;
+    const currentMonth = new Date().getMonth();
+    //get the total days of the current month
+    const totalDays = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate();
+    console.log("Total Days: ", totalDays);
+
+    let currentMonthCalendarValues = [];
+    for (let i = 0; i < totalDays; i++) {
+        currentMonthCalendarValues.push({
+            day: i + 1,
+            value: 0,
+        })
+    }
+    const data = {
+        userId: user?.id || "",
+        currentMonthCalendar: currentMonthCalendarValues,
+        currentMonth: currentMonth,
+    };
+
+    checkAndCreateDocument(db, collectionName, documentID, data);
+    }, [loading, session, user]);
 
 
-    return (
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="Starter" component={Starter} options={{ headerShown: false }} />
-                    <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
-                    <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
-                    <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
-                </Stack.Navigator>
-            </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Starter" component={Starter} options={{ headerShown: false }} />
+        <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
+        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "red",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
-
