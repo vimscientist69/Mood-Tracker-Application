c862c8defb170503f8161b3abd7b95136d6d8c99
William Ferns <willferns2000@gmail.com>
2024-01-17 06:23:34 +0200

    loader message on log in page fully integrated

--- components/SignInScreen.js
+++ components/SignInScreen.js
@@ -73,7 +73,6 @@ export default function SignInScreen() {
                             alignItems: 'center',
                             justifyContent: "center",
                             width: "100%",
-                            height: "100%",
                             gap: 20,
                         }}
                     >
