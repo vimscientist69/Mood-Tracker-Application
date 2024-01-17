115da12847e6ea8e676438fdfe873976defe7831
William Ferns <willferns2000@gmail.com>
2024-01-17 08:10:39 +0200

    done, now after this working on click functionality

--- components/BottomNavBar.js
+++ components/BottomNavBar.js
@@ -117,51 +117,47 @@ export default function BottomNavBar(props) {
       }
     }
 
-
     return (
         <View
             style={{
                 width: '100%',
                 display: "flex",
-                height: "100%",
                 backgroundColor: "#9949FF",
-                height: 106,
                 justifyContent: "center",
                 paddingVertical: 10,
                 paddingHorizontal: 22,
             }}
         >
-            {
-                updatingCurrentDayEmotion ? (
-                    <View
+            {updatingCurrentDayEmotion ? (
+                <View
+                    style={{
+                        display: "flex",
+                        alignItems: 'center',
+                        justifyContent: "center",
+                        width: "100%",
+                        gap: 20,
+                    }}
+                >
+                    <Text
                         style={{
-                            display: "flex",
-                            alignItems: 'center',
-                            justifyContent: "center",
-                            width: "100%",
-                            gap: 20,
+                            color: "#fff",
+                            fontWeight: "bold",
+                            fontSize: 20,
                         }}
                     >
-                        <Text
-                            style={{
-                                color: "#fff",
-                                fontWeight: "bold",
-                                fontSize: 20,
-                            }}
-                        >
-                            Updating Today's Mood...
-                        </Text>
-                        <ActivityIndicator size="large" color="#00ff00" />
-                    </View>
-                ) : (
+                        Updating Today's Mood...
+                    </Text>
+                    <ActivityIndicator size="large" color="#00ff00" />
+                </View>
+            ) : (
                     <View
                         style={{
+                            width: "100%",
                             display: "flex",
                             flexDirection: "row",
                             justifyContent: "space-between",
                             alignItems: "center",
                         }}
-
                     >
                         <View
                             style={{
@@ -174,12 +170,11 @@ export default function BottomNavBar(props) {
                         >
                             <TouchableOpacity
                                 onPress={() => {
-                                    updateTodaysMood(1, setReloadPage)
+                                    updateTodaysMood(1, setReloadPage);
                                 }}
                             >
-
                                 <Image
-                                    source={require('../assets/green-circle.png')} // Replace with the actual path to your image
+                                    source={require('../assets/green-circle.png')}
                                     style={{
                                         width: 44,
                                         height: 44,
@@ -189,41 +184,42 @@ export default function BottomNavBar(props) {
 
                             <TouchableOpacity
                                 onPress={() => {
-                                    updateTodaysMood(2, setReloadPage)
+                                    updateTodaysMood(2, setReloadPage);
                                 }}
                             >
                                 <Image
                                     style={{
-                                        width: 44, height: 44,
+                                        width: 44,
+                                        height: 44,
                                     }}
-                                    source={require('../assets/yellow-circle.png')} // Replace with the actual path to your image
+                                    source={require('../assets/yellow-circle.png')}
                                 />
                             </TouchableOpacity>
 
                             <TouchableOpacity
                                 onPress={() => {
-                                    updateTodaysMood(3, setReloadPage)
+                                    updateTodaysMood(3, setReloadPage);
                                 }}
                             >
-
                                 <Image
-                                    source={require('../assets/red-circle.png')} // Replace with the actual path to your image
+                                    source={require('../assets/red-circle.png')}
                                     style={{
                                         width: 44,
                                         height: 44,
                                     }}
                                 />
                             </TouchableOpacity>
-
                         </View>
 
                         <TouchableOpacity>
                             <Image
-                                source={require('../assets/stats.png')} // Replace with the actual path to your image
+                                source={require('../assets/stats.png')}
                                 style={{
+                                    // Add styles if needed
                                 }}
                             />
                         </TouchableOpacity>
+
                         <TouchableOpacity
                             style={{
                                 display: "flex",
@@ -235,27 +231,26 @@ export default function BottomNavBar(props) {
                                 alignItems: "center",
                                 gap: 10,
                                 borderRadius: 4,
-                                borderWidth: 2, // Border width
-                                borderColor: 'white', // Border color
+                                borderWidth: 2,
+                                borderColor: 'white',
                             }}
                             onPress={async () => {
                                 try {
                                     signOut();
-                                    navigation.navigate('SignIn')
+                                    navigation.navigate('SignIn');
                                 } catch (error) {
                                     console.error('Error clearing local storage:', error);
                                 }
                             }}
                         >
                             <Image
-                                source={require('../assets/signout.png')} // Replace with the actual path to your image
+                                source={require('../assets/signout.png')}
                                 style={{
                                 }}
                             />
                         </TouchableOpacity>
                     </View>
-                )
-            }
+                )}
         </View>
-    )
+    );
 }
